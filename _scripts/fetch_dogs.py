b96ccfaa3c826c9687f44bdcbd5e12ae656424ad
Joey <joey@pie.co.de>
2026-09-18 08:06:15 +0100

    try to fix 202 fail

--- _scripts/fetch_dogs.py
+++ _scripts/fetch_dogs.py
@@ -37,8 +37,10 @@ def build_session():
     retries = Retry(
         total=3,
         backoff_factor=0.5,
-        status_forcelist=[403, 429, 500, 502, 503, 504],
+        status_forcelist=[202, 403, 429, 500, 502, 503, 504],
         allowed_methods=["GET"],
+        respect_retry_after_header=True,
+        raise_on_status=False,
     )
     adapter = HTTPAdapter(max_retries=retries)
     session.mount("https://", adapter)
